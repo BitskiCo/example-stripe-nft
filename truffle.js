d89d8b41cc6df94fffce7b07aae26949224506c7
Josh Pyles <josh@outtherelabs.com>
2019-05-22 15:54:41 -0700

    Update bitski-node, use new ProviderManager with truffle

--- truffle.js
+++ truffle.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
-const Bitski = require('bitski-node');
+const { ProviderManager } = require('bitski-node');
 
-const credentials = {
-  id: process.env.BITSKI_APP_WALLET_ID,
-  secret: process.env.BITSKI_APP_WALLET_SECRET
-};
+const id = process.env.BITSKI_APP_WALLET_ID;
+const secret = process.env.BITSKI_APP_WALLET_SECRET;
 
-let providers = new Map();
+const providerManager = new ProviderManager(id, secret);
 
 module.exports = {
   networks: {
@@ -19,34 +17,19 @@ module.exports = {
     live: {
       network_id: '1',
       provider: () => {
-        if (providers.get("mainnet")) {
-          return providers.get("mainnet");
-        }
-        const provider = Bitski.getProvider(process.env.BITSKI_APP_WALLET_ID, { credentials: credentials });
-        providers.set("mainnet", provider);
-        return provider;
+        return providerManager.getProvider('mainnet');
       }
     },
     kovan: {
       network_id: '42',
       provider: () => {
-        if (providers.get("kovan")) {
-          return providers.get("kovan");
-        }
-        const provider = Bitski.getProvider(process.env.BITSKI_APP_WALLET_ID, { network: 'kovan', credentials: credentials });
-        providers.set("kovan", provider);
-        return provider;
+        return providerManager.getProvider('kovan');
       }
     },
     rinkeby: {
       network_id: '4',
       provider: () => {
-        if (providers.get("rinkeby")) {
-          return providers.get("rinkeby");
-        }
-        const provider = Bitski.getProvider(process.env.BITSKI_APP_WALLET_ID, { network: 'rinkeby', credentials: credentials });
-        providers.set("rinkeby", provider);
-        return provider;
+        return providerManager.getProvider('rinkeby');
       },
       gas: 4000000
     }
